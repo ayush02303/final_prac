ba2c54680f8610dd76ffc5193954217433f55914
ayush02303 <badolaayush295@gmail.com>
2026-06-16 06:46:45 +0530

    hii

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ function App() {
   return(
     <>
    <Posts/>
+   
     </>
   )
 }
--- src/CleanUp.jsx
+++ src/CleanUp.jsx
@@ -7,6 +7,7 @@ const CleanUp = () => {
     useEffect( ()=>{
         const Interval = setInterval( ()=>{
             setNum( prev => prev+1)
+            console.log("hii")
 
         }, 1000)
 
